07ac66f994f6e66095eb8d20999e49ba41cf6e64
Dimitrij Agal <dimitrij@traveloka.com>
2020-05-22 20:36:45 +0700

    add netlify CMS

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,23 @@
-import '../styles/index.css'
-import Head from 'next/head';
+import "../styles/index.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <link href="https://fonts.googleapis.com/css2?family=Open+Sans+Condensed:wght@700&family=Open+Sans:wght@300&display=swap" rel="stylesheet" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Open+Sans+Condensed:wght@700&family=Open+Sans:wght@300&display=swap"
+          rel="stylesheet"
+        />
+        <script
+          src="https://identity.netlify.com/v1/netlify-identity-widget.js"
+          async
+          defer
+        />
       </Head>
       <Component {...pageProps} />
     </>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
