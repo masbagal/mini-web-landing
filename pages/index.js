0e4da3f0b53252618884269180b78be7fcad4945
masbagal <dimitrij>
2020-05-29 18:05:39 +0700

    fix build and restyle hero template

--- pages/index.js
+++ pages/index.js
@@ -1,60 +1,7 @@
-import React, { useState } from "react";
-import TrayModal from "../components/TrayModal";
-import Slideshow from "../components/Slideshow";
+import React from "react";
 import MainLinkButton from "../components/MainLinkButton";
-import TrayButton from "../components/TrayButton";
 
 export default function IndexPage() {
-  const [showTray, toggleTray] = useState(false);
-
-  const items = [
-    {
-      title: "Pioneer Nasi Kulit",
-      content:
-        "Nasi Kulit Syuurga merupakan pioneer nasi kulit ayam dengan paru di Indonesia yang berdiri sejak 2018, kami berupaya menjaga dan memajukan makanan tradisional Indonesia.",
-      image: "/001.jpg",
-    },
-    {
-      title: "Lokasi Nasi Kulit Syuurga",
-      content:
-        "Persebaran nasi kulit syuurga di Indonesia ada hingga 30 titik di seluruh Indonesia",
-      image: "/002.jpg",
-    },
-    {
-      title: "Bermitra dengan Kami",
-      content:
-        "Kenali kami dulu dari produk kami, agar kita memiliki visi yang sama membangun bersama.",
-      image: "/003.jpg",
-    },
-    {
-      title: "Pioneer Nasi Kulit",
-      content:
-        "Nasi Kulit Syuurga merupakan pioneer nasi kulit ayam dengan paru di Indonesia yang berdiri sejak 2018, kami berupaya menjaga dan memajukan makanan tradisional Indonesia.",
-      image: "/001.jpg",
-    },
-    {
-      title: "Bermitra dengan Kami",
-      content:
-        "Kenali kami dulu dari produk kami, agar kita memiliki visi yang sama membangun bersama.",
-      image: "/003.jpg",
-    },
-  ];
-
-  const TrayContent = [
-    <TrayButton
-      key={1}
-      icon="/gofood.svg"
-      href="https://gofood.link/u/POQ7YA"
-      text="Pesan lewat Mamang GoFood"
-    />,
-    <TrayButton
-      key={2}
-      icon="/grab.png"
-      href="https://food.grab.com/id/id/restaurant/nasi-kulit-syuurga-caturtunggal-delivery/IDGFSTI00003anh"
-      text="Pesan lewat Abang GrabFood"
-    />,
-  ];
-
   return (
     <div>
       <div className="heroContainer">
@@ -64,12 +11,14 @@ export default function IndexPage() {
         ></div>
       </div>
 
-      <div className="flex flex-col px-8 mb-12 buttonContainer withHero">
-        <h1 className="logoTitle text-center mt-4 mb-8">
+      <div
+        className="flex flex-col px-8 buttonContainer text-white withHero pb-20"
+        style={{ background: "#d63447" }}
+      >
+        <h1 className="logoTitle text-center mt-4 mb-8 text-white">
           {"Nasi Kulit Syuurga"}
         </h1>
 
-        <button onClick={() => toggleTray(true)}>Pesan Sekarang!</button>
         <MainLinkButton
           href="https://drive.google.com/file/d/1UWj_jNVD-5IRAn44UqWPVMgtWUQI-tQg/view"
           text="Download Proposal Kemitraan"
@@ -79,14 +28,6 @@ export default function IndexPage() {
           text="Hubungi Kami"
         />
       </div>
-
-      <TrayModal
-        isShow={showTray}
-        handleClose={() => toggleTray(false)}
-        title="Pesan Sekarang!"
-        subtitle="Rasakan nikmatnya cita rasa Indonesia"
-        content={TrayContent}
-      />
     </div>
   );
 }
